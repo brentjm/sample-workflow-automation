ba70fcc8dfefadc4e3cbfea74ec3caa5e43e1851
Brent Maranzano <brent_maranzano@yahoo.com>
2022-06-03 07:29:59 -0400

    Removed duplicate files in ui-server

--- config.sh
+++ config.sh
@@ -11,15 +11,15 @@
 
 # Set the location of the UI homepage
 echo "UI homepage"
-sed -ie 's/REACT_APP_SERVERIP=/REACT_APP_SERVERIP="http:\/\/frigg\/swa-api"/' ui-server/react-app/.env
+sed -ie 's/  "homepage":.*/  "homepage": "http:\/\/frigg\/swa-ui",/' ui-server/react-app/package.json
 
 # Set the location of the backend server.
 echo "backend server"
-sed -ie 's/  "homepage":.*/  "homepage": "http:\/\/frigg\/swa-ui",/' ui-server/react-app/package.json
+sed -ie 's/REACT_APP_SERVERIP=.*/REACT_APP_SERVERIP="http:\/\/frigg\/swa-api"/' ui-server/react-app/.env
 
 # Set the proper name for keys and certificates in the NginX configuration
 echo "NinX.conf"
-sed -ie 's/  ssl_certificate .*/  ssl_certificate        \/etc\/nginx\/ssl\/swa.crt;/' ui-server/nginx.conf    
+sed -ie 's/  ssl_certificate.*/  ssl_certificate       \/etc\/nginx\/ssl\/swa.crt;/' ui-server/nginx.conf    
 sed -ie 's/  ssl_certificate_key.*/  ssl_certificate_key   \/etc\/nginx\/ssl\/swa.key;/' ui-server/nginx.conf    
 
 # Set the name of the certificate and key in the Dockerfile
